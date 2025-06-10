3fbdc4f01c3e062bbd5ad89ebc87d4c3985f3941
keerthi <reinekeerthi@gmail.com>
2025-06-10 19:31:00 +0200

    your message

--- database.py
+++ database.py
@@ -2,15 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# ✅ Temporarily hardcoded, test connection without .env
-DATABASE_URL="postgresql://postgres:123456789@localhost:5432/material_db"
+DATABASE_URL = "postgresql://postgres:123456789@localhost:5432/fastapi_db?client_encoding=utf8"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# def get_db():
 def get_db():
     db = SessionLocal()
     try:
